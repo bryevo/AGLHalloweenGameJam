a1cc51dd51e7e73e869cbd1f864bca44b928a3af
bryevo <ovnairb@gmail.com>
2017-10-28 19:09:55 -0700

    Hotel room w/ onClick

---
+++ Assets/BedController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoToSleep()
+	{
+		Debug.Log("Go to sleep!");
+	}
+}
