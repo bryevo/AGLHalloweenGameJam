2f864dfa2f049e76254752e5673a37a435fedcbb
Kevin Do <kevindokhoale@gmail.com>
2017-10-29 18:55:25 -0700

    oni

---
+++ Assets/Scripts/OniController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OniController : MonoBehaviour {
+
+
+	public float speed = 5f;
+	public Rigidbody2D player;
+	Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerDir = player.velocity;
+		float t = 1f;
+
+		Vector3 displacement = (playerDir * t) + player.transform.position;
+		Vector3 dir = displacement - transform.position;
+		transform.position += dir.normalized * speed * Time.deltaTime;
+	}
+}
